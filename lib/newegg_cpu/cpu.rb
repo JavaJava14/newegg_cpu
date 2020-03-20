b67d392f976b91913fd10671989a0de88b222223
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-03-19 20:56:55 -0700

    Update cpu.rb
    Add attributes and array, then push attributes to array. Create all method to get all elements from array.

--- lib/newegg_cpu/cpu.rb
+++ lib/newegg_cpu/cpu.rb
@@ -1,7 +1,16 @@
 class NeweggCpu::Cpu
 
+    attr_accessor :name, :price, :url
 
-
-
-
+    @@all = []
+  
+    def initialize(name,price,url)
+      @name = name
+      @price = price
+      @url = url
+      @@all << self
+    end 
+    def self.all 
+      @@all
+    end
 end
