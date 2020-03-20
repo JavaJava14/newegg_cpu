0967ea07fa701a4895b711b9e0e73ec09934f9b8
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-03-19 21:05:43 -0700

    Update scraper.rb
    Parse url with Nokogiri. Also gets items from site.

--- lib/newegg_cpu/scraper.rb
+++ lib/newegg_cpu/scraper.rb
@@ -1,8 +1,9 @@
 class NeweggCpu::Scraper
 
+  def self.get_cpu
+    url = "https://www.newegg.com/p/pl?Submit=ENE&IsNodeId=1&N=100007671%2050001028&cm_sp=Cat_CPU-Processors_2-_-Visnav-_-AMD-CPU_2"
+    unparsed = HTTParty.get(url) #Raw HTML
+    parsed = Nokogiri::HTML(unparsed) #Readable format
+    processors_list = parsed.css('div.item-container')#36 items
 
-
-
-
-    
 end
