d807449d53139d601a183f06cf06ae04e07b7b82
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-03-19 21:33:46 -0700

    Update cli.rb
    Added select feature for users to choose from generated list .

--- lib/newegg_cpu/cli.rb
+++ lib/newegg_cpu/cli.rb
@@ -15,6 +15,40 @@ class NeweggCpu::CLI
     NeweggCpu::Scraper.get_cpu
   end
 
+  def select_product
+    puts ""
+    puts "Enter any number from 1 through 36 to learn about the product"
+    input = gets.chomp.to_i
+    if input > 0 && input <= NeweggCpu::Cpu.all.length# 1-36
+     NeweggCpu::Cpu.all[input - 1].tap do |item|
+      puts ""
+      puts "#{item.name}".gsub(":name=>","")
+      puts "#{item.price}".gsub(/["{:price=>\\nt–}"]/, "").strip
+      puts "#{item.url}".gsub(":url=>","").strip
+      select_product_continued
+     end
+    else 
+      puts ""
+      puts "You must enter a valid number"
+      select_product
+    end 
+
+    def select_product_continued
+      puts ""
+      puts "Enter list to view info on featured items or menu"
+      input = gets.chomp
+      case input.downcase
+        when "menu" 
+          puts ""
+          puts "Enter list to view info on featured items."
+          menu
+        when "list"
+          select_product
+        else
+        puts "Invalid response"
+        select_product_continued
+      end
+
   def menu
     puts "Enter search to find featured items."
     puts "Enter exit to exit program."
@@ -23,7 +57,9 @@ class NeweggCpu::CLI
       when "search"
         puts ""
         generate_list
+        select_product
       when "list"
+        select_product
       when "exit"
         puts ""
         puts "come back soon!"
