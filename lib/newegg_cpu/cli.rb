0adf7cb1a41b93b3a514650473be0cb2816b7a01
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-03-19 20:49:37 -0700

    Update cli.rb
    Added greet method to greet user.

--- lib/newegg_cpu/cli.rb
+++ lib/newegg_cpu/cli.rb
@@ -1,8 +1,13 @@
 class NeweggCpu::CLI
 
   def call
-
+    greet
   end
 
-
+  def greet
+    puts "\nNewegg featured items from Desktop Processors"
+    puts "\nWelcome"
+    puts ""
+  end
+  
 end
