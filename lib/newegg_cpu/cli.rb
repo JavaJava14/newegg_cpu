33bc34bfc34fc88c1d88ac39b4638fb7d8a03442
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-03-19 21:31:07 -0700

    Update cli.rb
    Added menu for user to grab input. When they input "search" then it will call generate_list. Also added exit feature.

--- lib/newegg_cpu/cli.rb
+++ lib/newegg_cpu/cli.rb
@@ -2,6 +2,7 @@ class NeweggCpu::CLI
 
   def call
     greet
+    menu
   end
 
   def greet
@@ -14,4 +15,24 @@ class NeweggCpu::CLI
     NeweggCpu::Scraper.get_cpu
   end
 
+  def menu
+    puts "Enter search to find featured items."
+    puts "Enter exit to exit program."
+    input = gets.chomp
+    case input.downcase
+      when "search"
+        puts ""
+        generate_list
+      when "list"
+      when "exit"
+        puts ""
+        puts "come back soon!"
+        exit
+      else
+        puts ""
+        puts "Sorry, that input is not recognized."
+        puts ""
+        menu
+    end
+  end
 end
